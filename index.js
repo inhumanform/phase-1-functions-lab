3e20565165268747a4c99be9dc2d7265b3e699c5
Drew <dch528@gmail.com>
2024-01-29 21:52:32 -0500

    Drew Hairston Phase 1 Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blockNumber) {
+    return Math.abs(blockNumber - 42);
+}
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+
+}
+
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    if (distanceInFeet <= 400) {
+        return 0; 
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+        return (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+        return 25; 
+    } else {
+        return "cannot travel that far";
+    }
+}
